551e8f5a6b687305fedb78819f7e6fe3803a5481
Ardor <itmazimeman@gmail.com>
2020-08-07 13:15:56 +0900

    commit 1

---
+++ Main/src/A00002_20_08_06_10718.java
@@ -0,0 +1,8 @@
+public class A00002_20_08_06_10718 {
+
+	public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+	}
+
+}
---
+++ Main/src/A00003_20_08_06_10171.java
@@ -0,0 +1,9 @@
+public class A00003_20_08_06_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
